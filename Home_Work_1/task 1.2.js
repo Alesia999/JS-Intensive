126c0618050b8b5ebe229d8979a9aee0320df3d3
Alesia999 <73301282+Alesia999@users.noreply.github.com>
2022-03-09 13:54:56 +0100

    refactor: task 1.1

--- Home_Work_1/task 1.2.js
+++ Home_Work_1/task 1.2.js
@@ -2,17 +2,17 @@ function showResult(firstNumber, secondNumber) {
   const RES1 = Number(firstNumber) + Number(secondNumber);
   const RES2 = Number(firstNumber) / Number(secondNumber);
 
-  console.log( `Ответ: ${RES1}, ${RES2}.` );
+  console.log(`Ответ: ${RES1}, ${RES2}.`);
 }
 
-const NUM1 = prompt( 'Введите число' );
+const NUM1 = prompt('Введите число');
 const NUM1_IS_VALID = !isNaN(Number(NUM1)) && NUM1 !== '' && Number(NUM1) !== Infinity;
 
 if (!NUM1_IS_VALID) {
-  console.log( 'Некорректный ввод!' );
+  console.log('Некорректный ввод!');
 } else {
-  const NUM2 = prompt( 'Введите второе число' );
+  const NUM2 = prompt('Введите второе число');
   const NUM2_IS_VALID = Number(NUM2) && Number(NUM2) !== Infinity;
 
-  !NUM2_IS_VALID ? console.log( 'Некорректный ввод!' ) : showResult(NUM1, NUM2);
+  !NUM2_IS_VALID ? console.log('Некорректный ввод!') : showResult(NUM1, NUM2);
 }
